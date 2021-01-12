28ac1ade73b73f53d51a45e7c248ca5d551e1d23
skendanavian <s.nissen.bass@gmail.com>
2021-01-11 18:46:28 -0600

    challenge solution for New Year Chaos

--- arrays/NewYearChaos.js
+++ arrays/NewYearChaos.js
@@ -5,31 +5,27 @@
 // Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took place to get the queue into its current state. If anyone has bribed more than two people, the line is too chaotic to compute the answer.
 
 function minimumBribes(line) {
-  // if number is more than 2 less than index -- too chaotic
-  // if number is 1 OR 2 less than index -- add to counter
   let counter = 0;
-  for (let i in line) {
+  for (let i = line.length - 1; i >= 0; i--) {
     if (line[i] - i > 3) {
       console.log("Too chaotic");
       return;
     }
-    if (line[i] - i <= 3 && line[i] - i > 0) {
-      line[i] - 1 === 0
-        ? (counter += line[i] - i)
-        : (counter += line[i] - i - 1);
-      console.log(
-        "index: " + i + " num: " + line[i] + " current counter: " + counter
-      );
+    for (let n = line[i] - 2; n < i; n++) {
+      if (line[n] > line[i]) counter++;
     }
   }
   console.log(counter);
 }
 
+// Test Input
+
 const array1 = [2, 1, 5, 3, 4];
 const array2 = [2, 5, 1, 3, 4];
 const array3 = [1, 2, 5, 3, 7, 8, 6, 4];
-const array3 = [1, 2, 5, 3, 4, 7, 8, 6];
+const array4 = [1, 2, 5, 3, 4, 7, 8, 6];
 
 minimumBribes(array1); // expect 3
 minimumBribes(array2); // expect 'Too chaotic'
 minimumBribes(array3); // expect 7
+minimumBribes(array4); // expect 4
