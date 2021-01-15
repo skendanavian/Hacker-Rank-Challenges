afa9b32d9d2484b61c8d88622ab77e843f848f67
skendanavian <s.nissen.bass@gmail.com>
2021-01-14 21:11:14 -0600

    setup minimum swaps 2 challenge

---
+++ arrays/minimumSwaps2.js
@@ -0,0 +1,14 @@
+// You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+
+function minimumSwaps(arr) {
+  // store indexes of highest and lowest numbers which are not in correct position
+  // loop
+}
+
+// Test input
+
+const array1 = [4, 3, 1, 2];
+const array2 = [1, 3, 5, 2, 4, 6, 7];
+
+minimumSwaps(array1); // expect 3
+minimumSwaps(array2); // expect 3
