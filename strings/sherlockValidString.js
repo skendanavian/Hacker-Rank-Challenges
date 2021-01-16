31c36c5d5dfc5fe183dccece292c4ba0976ade8e
skendanavian <s.nissen.bass@gmail.com>
2021-01-15 20:21:22 -0600

    new refactor solves all but 1 edge case on hacker rank

--- strings/sherlockValidString.js
+++ strings/sherlockValidString.js
@@ -1,69 +1,58 @@
 // Sherlock considers a string to be valid if all characters of the string appear the same number of times. It is also valid if he can remove just 1  character at 1 index in the string, and the remaining characters will occur the same number of times. Given a string , determine if it is valid. If so, return YES, otherwise return NO.
 
 function isValid(s) {
-  // convert to array
-  // sort
-  // remove duplicate characters
-  // length of removed duplicates should now be exactly 1/2 or 1/2 + 1 of original.
-
-  const lettersArray = s.split("").sort();
+  const lettersArray = s.split("");
   const countObj = {};
 
+  // assign each character to object key/count
   lettersArray.forEach((e) => {
     const count = countObj[e] >= 0 ? countObj[e] + 1 : 0;
     countObj[e] = count;
   });
 
-  let extraChars = 0;
-  let allEven = true;
+  // Track Various Possible outcomes
+  let oddGroupCount = 0;
+  let allCountsAreEven = true;
+  let evenCharCount = 0;
+  let singleCharCount = 0;
 
-  for (char in countObj) {
-    if (countObj[char] % 2 !== 0) {
-      extraChars++;
-      allEven = false;
+  // loop through charObj -> adjust outcome counters accordingly
+  for (let char in countObj) {
+    if (countObj[char] % 2 !== 0 && countObj[char] !== 0) {
+      evenCharCount++;
+      allCountsAreEven = false;
+    } else if (countObj[char] % 2 === 0 && countObj[char] !== 0) {
+      oddGroupCount++;
+    }
+    if (countObj[char] === 0) {
+      singleCharCount++;
+      allCountsAreEven = false;
     }
   }
+  //PASSING CASES
+  // 1 case of an odd number group of letters AND no single characters
+  // no odd number groups AND 1 single character
+  // All letter groups contain even numbers
+  // Every letter is a single case
+  // console.log({ countObj });
+  // console.log({ singleCharCount });
+  // console.log({ oddGroupCount });
+  // console.log({ allCountsAreEven });
+  // console.log({ evenCharCount });
 
-  let duplicates = [];
-  let remainder = s.length;
-  let individuals = 0;
-
-  // for (let i = 0; i < lettersArray.length; i += 2)
-  let i = 0;
-  while (i < lettersArray.length) {
-    console.log(i);
-    if (lettersArray[i] === lettersArray[i + 1]) {
-      duplicates = [...duplicates, lettersArray[i]];
-      remainder -= 2;
-      i += 2;
-    } else {
-      i += 1;
-      individuals++;
-    }
+  if (
+    (!singleCharCount && oddGroupCount <= 1) ||
+    (!oddGroupCount && singleCharCount <= 1) ||
+    singleCharCount === s.length ||
+    allCountsAreEven ||
+    (!evenCharCount && singleCharCount === 1)
+  ) {
+    console.log("YES");
+    return "YES";
+  } else {
+    console.log("NO");
+    return "NO";
   }
-
-  // for (let i = 0; i < 20; i++) {
-  //   console.log(lettersArray[i]);
-  //   console.log(duplicates[i]);
-  // }
-
-  // console.log({ duplicates });
-  // console.log(duplicates.length);
-  // console.log({ lettersArray });
-  console.log({ countObj });
-  // console.log(lettersArray.length);
-  // console.log({ remainder });
-  // console.log({ individuals });
-  // console.log({ lettersArray });
-
-  // if (
-  //   lettersArray.length - duplicates.length * 2 <= 1 ||
-  //   duplicates.length < 2
-  // ) {
-  //   console.log("YES");
-  // } else {
-  //   console.log("NO");
-  // }
 }
 
 // test input
@@ -75,8 +64,8 @@ const string4 = "aaaaabc";
 const string5 =
   "ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd";
 
-// isValid(string1); // expect NO
-// isValid(string2); // expect YES
-// isValid(string3); // expect NO
-// isValid(string4); // expect NO
+isValid(string1); // expect NO
+isValid(string2); // expect YES
+isValid(string3); // expect NO
+isValid(string4); // expect NO
 isValid(string5); // expect YES
